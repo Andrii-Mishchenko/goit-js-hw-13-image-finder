2ce95178e2bbd1369b54d800aadddaf7de5781cf
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-04-25 22:20:04 +0300

    render of pictures completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import './styles.css';
+import cardTpl from './js/cardTpl.hbs'
+import PicturesApiService from './js/apiService'
+import LoadMoreBtn from './js/load-more-btn';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    pictureContainer: document.querySelector('.gallery'),
+    // loadMoreBtn: document.querySelector('[data-action="load-more"]')
+}
+
+const picturesApiService = new PicturesApiService();
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+
+
+function onSearch(e){
+    e.preventDefault();
+
+    loadMoreBtn.show();  
+
+    picturesApiService.query = e.currentTarget.elements.query.value;
+    picturesApiService.resetPage();
+    clearPicturesContainer();
+
+    // loadMoreBtn.disable();
+    // picturesApiService.fetchPictures()
+    // .then(pictures =>{    
+    //     picturesPageMurkup(pictures);    
+    //     loadMoreBtn.enable();
+    // });    
+    onLoadMore()
+}
+
+function onLoadMore(){
+    loadMoreBtn.disable()
+    picturesApiService.fetchPictures()
+    .then(pictures => {
+        picturesPageMurkup(pictures);
+        // window.scrollTo({
+        //     top: 1000,
+        //     behavior: "smooth"
+        // });
+        
+        loadMoreBtn.enable();
+    });
+}
+
+function picturesPageMurkup(picturesArray){
+    const murkup = cardTpl(picturesArray);
+    refs.pictureContainer.insertAdjacentHTML('beforeend', murkup)
+}
+
+function clearPicturesContainer(){
+    refs.pictureContainer.innerHTML = '';
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,44 @@
+const API_KEY ='21204345-b994baef221a75cda7aa18f50';
+const BASE_URL = 'https://pixabay.com/api'
+
+export default class PicturesApiService {
+    constructor(){
+        this.searchQuery = '';
+        this.page = 1;
+        
+    }
+
+    fetchPictures(){
+        console.log(this);
+        
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+        return fetch(url)
+        .then(response => response.json())
+        .then(pictures =>{
+            console.log(pictures);
+            this.incrementPage();
+
+            const picturesArray = pictures.hits;
+            return picturesArray;
+        })
+        .catch(error => console.log(error));
+
+    }
+
+    incrementPage(){
+        this.page +=1;
+    }
+
+    resetPage(){
+        this.page = 1;
+    }
+
+    get query(){
+        return this.searchQuery;
+    }
+
+    set query(newQuery){
+        this.searchQuery = newQuery;
+    }
+};
